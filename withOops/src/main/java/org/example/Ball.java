c401a6da7900c853d297d7823e80fd3ae5090274
Yuthika9 <167095094+Yuthika9@users.noreply.github.com>
2024-04-22 10:09:39 +0530

    Update Ball.java

--- withOops/src/main/java/org/example/Ball.java
+++ withOops/src/main/java/org/example/Ball.java
@@ -4,21 +4,21 @@ import processing.core.PApplet;
 
 public class Ball {
 
-    private int x_cord;
-    private int y_cord;
-    private int frame;
+    private float x_cord;
+    private float y_cord;
+    private float frame;
 
-    public Ball(int x_cord, int y_cord, int diameter, int frame) {
+    public Ball(float x_cord, float y_cord, float diameter, float frameRate) {
 
         this.x_cord = x_cord;
         this.y_cord = y_cord;
         this.diameter = diameter;
-        this.frame = frame;
+        this.frameRate = frameRate;
     }
 
     public void drawCircle(PApplet applet) {
 
         applet.ellipse(x_cord, y_cord, diameter, diameter);
-        x_cord = x_cord + frame;
+        x_cord = x_cord + frameRate;
     }
 }
